1d486f59392f6dc272899fdfd88fa1c48eec3b2d
Euan <euank@euank.com>
2015-12-23 00:14:19 -0800

    Retab

--- module-manager.js
+++ module-manager.js
@@ -35,27 +35,27 @@ function quotedSplit(str) {
 }
 
 function stringifyArgs(args) {
-	var strParts = [];
-	for(var i=0;i<arguments.length;i++) {
-		if(typeof arguments[i] === 'string') {
-			strParts.push(arguments[i]);
-		} else if(Array.isArray(arguments[i])) {
-			strParts = strParts.concat(stringifyArgs.apply(this, arguments[i]));
-		} else if(arguments[i] === undefined || arguments[i] === null) {
-			strParts.push('');
-		} else{
-			strParts.push(arguments[i].toString());
-		}
-	}
-	return strParts;
+  var strParts = [];
+  for(var i=0;i<arguments.length;i++) {
+    if(typeof arguments[i] === 'string') {
+      strParts.push(arguments[i]);
+    } else if(Array.isArray(arguments[i])) {
+      strParts = strParts.concat(stringifyArgs.apply(this, arguments[i]));
+    } else if(arguments[i] === undefined || arguments[i] === null) {
+      strParts.push('');
+    } else{
+      strParts.push(arguments[i].toString());
+    }
+  }
+  return strParts;
 }
 
 var config = null;
 
 
 module.exports = function(conf, irc) {
-	config = conf;
-	return moduleManager;
+  config = conf;
+  return moduleManager;
 };
 
 var supportedConfigTypes = [
@@ -85,55 +85,55 @@ var supportedConfigTypes = [
 
 
 function moduleManager(irc) {
-	var managerFns = {
-		sayTo: function(to, args) {
-			var strs = stringifyArgs(args);
-			irc.send(to, stringifyArgs(args).join(' '));
-		},
-		say: function(args) {
-			args = Array.prototype.slice.call(arguments);
-			managerFns.sayTo.apply(this, [config.mainChannel].concat(args));
-		},
-		getConfig: function(name, cb) {
-			var fullPath = path.join(config.configfolder, name);
-			var done = false;
-			fs.readFile(fullPath,{encoding: 'utf8'}, function(err, res) {
-				if(err) return cb(err);
+  var managerFns = {
+    sayTo: function(to, args) {
+      var strs = stringifyArgs(args);
+      irc.send(to, stringifyArgs(args).join(' '));
+    },
+    say: function(args) {
+      args = Array.prototype.slice.call(arguments);
+      managerFns.sayTo.apply(this, [config.mainChannel].concat(args));
+    },
+    getConfig: function(name, cb) {
+      var fullPath = path.join(config.configfolder, name);
+      var done = false;
+      fs.readFile(fullPath,{encoding: 'utf8'}, function(err, res) {
+        if(err) return cb(err);
 
-				var ext = path.extname(fullPath);
-				supportedConfigTypes.forEach(function(type) {
-					if(_.any(type.exts, function(e) { return e === ext; })) {
-						type.parse(res, fullPath, cb);
-						done = true;
-						return false;
-					}
-				});
-				if(!done) return cb(null, res);
-			});
-		},
-	};
+        var ext = path.extname(fullPath);
+        supportedConfigTypes.forEach(function(type) {
+          if(_.any(type.exts, function(e) { return e === ext; })) {
+            type.parse(res, fullPath, cb);
+            done = true;
+            return false;
+          }
+        });
+        if(!done) return cb(null, res);
+      });
+    },
+  };
 
-	// Create our server on a well-known port
-	var server = new Bjson.NamedTcpServer("0.0.0.0", 52531, managerFns, {buffer: true}, function(err) {
-		if(err) {
-			winston.error("Could not create NamedTcpServer", {err: err});
-			return;
-		}
-		// Woo!
-		irc.on('message', function(msg) {
-			server.broadcast("irc_message", msg);
-			var primaryFrom = (msg.to == irc.me) ? msg.from : msg.to;
-			var text = msg.message;
-			if(text == "!LIST_CLIENTS") console.log(server);
-			if(text.substring(0, config.commandPrefix.length) == config.commandPrefix) {
-				var re = new RegExp('^' + reEscape(config.commandPrefix) + '(\\S*)\\s*(.*)$', 'g');
-				var rem = re.exec(text);
-				var command = rem[1];
-				var remainder = rem.length == 3 ? rem[2] : "";
-				var respTo = primaryFrom;
+  // Create our server on a well-known port
+  var server = new Bjson.NamedTcpServer("0.0.0.0", 52531, managerFns, {buffer: true}, function(err) {
+    if(err) {
+      winston.error("Could not create NamedTcpServer", {err: err});
+      return;
+    }
+    // Woo!
+    irc.on('message', function(msg) {
+      server.broadcast("irc_message", msg);
+      var primaryFrom = (msg.to == irc.me) ? msg.from : msg.to;
+      var text = msg.message;
+      if(text == "!LIST_CLIENTS") console.log(server);
+      if(text.substring(0, config.commandPrefix.length) == config.commandPrefix) {
+        var re = new RegExp('^' + reEscape(config.commandPrefix) + '(\\S*)\\s*(.*)$', 'g');
+        var rem = re.exec(text);
+        var command = rem[1];
+        var remainder = rem.length == 3 ? rem[2] : "";
+        var respTo = primaryFrom;
 
-				server.broadcast("command", [command, remainder, quotedSplit(remainder), respTo, msg.from, msg.to, text]);
-			}
-		});
-	});
+        server.broadcast("command", [command, remainder, quotedSplit(remainder), respTo, msg.from, msg.to, text]);
+      }
+    });
+  });
 }
